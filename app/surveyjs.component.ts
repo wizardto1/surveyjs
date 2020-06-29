5a4090429beeeef066b4af2038909d5de0bbd480
wizardto1 <ph429@drexel.edu>
2020-06-29 13:16:35 -0400

    1 file modified

--- app/surveyjs.component.ts
+++ app/surveyjs.component.ts
@@ -518,7 +518,6 @@ export class SurveyjsComponent  {
  requiredText: "",
  firstPageIsStarted: true,
  showPreviewBeforeComplete: "showAnsweredQuestions",
- maxTimeToFinishPage: 10000,
  showTimerPanel: "top",
  showTimerPanelMode: "page"
 };
